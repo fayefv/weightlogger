4ab59847b3dc0226af002d89c3bd4ce69f318101
fayefv <fong.faye@gmail.com>
2021-02-28 14:15:23 -0800

    made UI prettier

--- GUIWindow.py
+++ GUIWindow.py
@@ -1,12 +1,6 @@
 """A GUI version of WeightLogger application.
 
 @author fayefong
-
-TODO bind widget events appropriately
-TODO documentation
-TODO hierarchy
-TODO comprehension review
-
 """
 import tkinter as tk
 from tkcalendar import Calendar, DateEntry
@@ -18,30 +12,18 @@ import controller as ct
 from matplotlib.dates import DateFormatter
 
 
-# from functools import partial
-# import logger as lg
-# import csv
-
-# from datetime import date, datetime
-# import matplotlib
-# import matplotlib.dates as mdates
-
-# import os
-# matplotlib.use("TkAgg")
-
-
 class App(tk.Tk):
     def __init__(self):  # widgets are attributes of the class
         super().__init__()
-        self.l1 = tk.Label(self, text="Date", font=("Arial", 25)).grid(row=0)
-        self.l2 = tk.Label(self, text="Weight (lbs)", font=("Arial", 25)).grid(row=1)
+        self.l1 = tk.Label(self, text="Date", font=("Arial", 25)).grid(row=0, column=0, sticky=tk.E)
+        self.l2 = tk.Label(self, text="Weight (lbs)", font=("Arial", 25)).grid(row=1, column=0, sticky=tk.E)
 
-        self.cal = DateEntry(self, font=("Arial", 20))  # returns a str (M/D/YY)
-        self.cal.grid(row=0, column=1, padx=30, pady=10, ipady=10)
+        self.cal = DateEntry(self, font=("Arial", 20), width=8)  # returns a str (M/D/YY)
+        self.cal.grid(row=0, column=1, padx=30, pady=10, ipady=10, sticky=tk.W )
         self.cal.bind("<<DateEntrySelected>>", self.fill_next)  # binds date selected to fill entry with record
 
-        self.e_w = tk.Entry(self, font=("Arial", 20))
-        self.e_w.grid(row=1, column=1, padx=30, pady=10, ipady=10)
+        self.e_w = tk.Entry(self, font=("Arial", 20), width=8)
+        self.e_w.grid(row=1, column=1, padx=30, pady=10, ipady=10, sticky=tk.W )
         # on startup, autofill today's date and weight
         self.auto_fill()
         # bind keypress events for intuitive data entry
@@ -49,24 +31,27 @@ class App(tk.Tk):
 
         button_style = font.Font(family="Arial", size=25, weight='bold')
         self.quit_btn = tk.Button(self, text="Quit", font=button_style, command=self.quit)
-        self.quit_btn.grid(row=3, column=0, sticky=tk.W, pady=10, padx=100)
+        self.quit_btn.grid(row=3, column=0, pady=10, padx=50, ipadx=20, ipady=10)
+
+        self.report_btn = tk.Button(self, text='Report', font=button_style, command=self.send_report)
+        self.report_btn.grid(row=4, column=0, pady=10, padx=50, ipady=10)
+
         self.submit_btn = tk.Button(self, text="Submit", font=button_style, command=self.submit_handler)
-        self.submit_btn.grid(row=3, column=1, sticky=tk.W, pady=10, padx=100)
+        self.submit_btn.grid(row=3, column=1, pady=10, padx=30, sticky=tk.W, ipadx=50, ipady=10)
 
         # user is likely to write a new value in the entry and hit plot
         # clicking plot button should update the log and redraw the graph
         # combine funcs and bind to plot button
         self.plot_btn = tk.Button(self, text="Plot", font=button_style,
                                   command=self.combine_funcs(self.submit_handler, self.update_graph))
-        self.plot_btn.grid(row=4, column=1, sticky=tk.W, pady=10, padx=100)
+        self.plot_btn.grid(row=4, column=1, pady=10, padx=30, sticky=tk.W, ipadx=75, ipady=10)
+
+        # user statistics printout
 
 
-        self.report_btn = tk.Button(self, text='Send Report', font=button_style, command=self.send_report)
-        self.report_btn.grid(row=4, column=0, sticky=tk.W, pady=10, padx=100)
 
         # embed empty plot for startup view
         self.initialize_graph()
-
 
     def combine_funcs(self, *funcs):
         def inner_combined_func(*args, **kwargs):
@@ -77,12 +62,6 @@ class App(tk.Tk):
         # this reference will have the called result of all
         # the functions that are passed to the combined_funcs
         return inner_combined_func
-
-    def func1(self):
-        print("function 1")
-
-    def func2(self):
-        print("function 2")
 
     def handle_keypress(self, e):
         if e.char == '\r':  # log new weight when user hits <Enter>
@@ -112,14 +91,14 @@ class App(tk.Tk):
         self.figure = Figure(figsize=(9, 8), dpi=100)
         self.plt = self.figure.add_subplot(1, 1, 1)
         self.canvas = FigureCanvasTkAgg(self.figure, self)
-        self.canvas.get_tk_widget().grid(row=0, column=2, rowspan=15, padx=5, pady=5)
+        self.canvas.get_tk_widget().grid(row=0, column=2, rowspan=15, columnspan=10, padx=5, pady=5)
         self.plt.set_title("Weight Change over Time")
 
     def update_graph(self):
         self.set_up_graph()
         # since no more lazy loading of data entry
         # actually CSV file is always kept up to date
-        x, y = ct.update_graph()
+        x, y = ct.get_records()
 
         self.plt.plot(x, y, color='lightgray', marker='o', markerfacecolor='black')
         self.plt.set_xlabel('Date')
