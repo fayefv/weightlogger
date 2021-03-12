0e1b22ec920c009c5083abb1ac7d17b81aac0baa
fayefv <fong.faye@gmail.com>
2021-03-12 12:43:42 -0800

    new package build with demo image

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wlg_fayefv", # Replace with your own username
-    version="0.1.7",
+    version="0.1.8",
     install_requires=requirements,
     author="Faye Fong",
     author_email="fong.faye@gmail.com",
