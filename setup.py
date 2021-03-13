ad3a37c320d27decfaf1c270b98d502c6a518575
fayefv <fong.faye@gmail.com>
2021-03-13 01:49:48 -0800

    stable data folder

--- setup.py
+++ setup.py
@@ -15,14 +15,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="wlg_fayefv", # Replace with your own username
-    version="0.2.0",
+    version="0.2.2",
     install_requires=requirements,
     author="Faye Fong",
     author_email="fong.faye@gmail.com",
     description="A simple weightlogging app to track personal fitness.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/fayefv/weightlogger/tree/prettyGUI",
+    url="https://github.com/fayefv/weightlogger",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
